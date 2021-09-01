cbeb2ecfcc9039ddec6e0741cee4d45adacebb1b
ShirleyNgiam <86401778+ShirleyNgiam@users.noreply.github.com>
2021-09-01 13:43:01 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,19 @@
-function compute()
+function compute(p,r,y)
 {
-    p = document.getElementById("principal").value;
     
-}
-        
+    var p = document.getElementById("principal").value;
+    var r = document.getElementById("rate").value;
+    var y = parseInt(document.getElementById("years").value);
+    var yearInFuture = new Date().getFullYear() + y;
+    var result = p * r * 0.01 * y;
+    document.getElementById("displayOut").innerHTML = "If you deposit <mark>" + p + "</mark>,<br/>" +
+    "at an interest rate of <mark>" + r + "&#37" + "</mark>,<br/>" +
+    "You will receive an amount of <mark>" + result + "</mark>,<br/> " +
+    "in the year <mark>" + yearInFuture + "</mark>"; 
+    // document.getElementById("iRate").innerHTML = "<p> r #&#37  </p> </br>" ;
+
+}    
+//get range value
+function getRangeValue() {
+    document.getElementById("iRate").innerHTML = document.getElementById("rate").value + "&#37";
+}    
